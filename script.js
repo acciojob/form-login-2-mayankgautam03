4f09770c5ed062679c2c4f7aafeafbae1eff5f6d
Mayank Kumar Gautam <mayank.guts30@gmail.com>
2025-03-12 22:39:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//your JS code here. If required.
-function showalert () {
- let firstName = document.getElementById("firstName").value;
-            let lastName = document.getElementById("lastName").value;
-            let phoneNumber = document.getElementById("phoneNumber").value;
-            let emailId = document.getElementById("emailId").value;
-	let output= `First Name:${firstName} Last Name:${lastName} phone Number:${phoneNumber} Email ID:${emailId}`;
+// //your JS code here. If required.
+// function showalert () {
+//  let firstName = document.getElementById("firstName").value;
+//             let lastName = document.getElementById("lastName").value;
+//             let phoneNumber = document.getElementById("phoneNumber").value;
+//             let emailId = document.getElementById("emailId").value;
+// 	let output= `First Name:${firstName} Last Name:${lastName} phone Number:${phoneNumber} Email ID:${emailId}`;
 
-	alert(output);
-}
+// 	alert(output);
+// }
